305c759fbe68f85fa282cafbdc4d349299aeb2a5
elikemdanielsdev <elikemdaniels@gmail.com>
2024-11-01 19:39:23 +0000

    updated config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,12 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: [\n          'Inter"',\n          "ui-sans-serif",\n          "system-ui",\n          "sans-serif",\n          'Apple Color Emoji"',\n          'Segoe UI Emoji"',\n          'Segoe UI Symbol"',\n          'Noto Color Emoji"',\n        ]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
-  },
+  darkMode: ["class"],
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  theme: {},
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
-      require("tailwindcss-animate")
-],
+    require("tailwindcss-animate"),
+  ],
 } satisfies Config;
