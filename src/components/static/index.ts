4c8ff743114f59af7ce24b165f42f91f740d882a
elikemdanielsdev <elikemdaniels@gmail.com>
2024-12-09 06:19:23 +0000

    General update

---
+++ src/components/static/index.ts
@@ -0,0 +1,8 @@
+import Section from "@/components/static/Section.astro";
+import Container from "@/components/static/Container.astro";
+import Heading from "@/components/static/HeadingText.astro";
+import Header from "@/components/static/Header.astro";
+import Footer from "@/components/static/Footer.astro";
+import Link from "@/components/static/Link.astro";
+
+export { Section, Container, Heading, Header, Footer, Link };
